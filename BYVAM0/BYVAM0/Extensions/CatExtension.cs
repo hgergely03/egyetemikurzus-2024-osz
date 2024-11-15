f0c94a4a4d926d401cd1d8703124e697ae5301b8
Horváth Gergely <gergely0703@gmail.com>
2024-11-15 20:06:08 +0100

    feat: Add Cat model and related assets

---
+++ BYVAM0/BYVAM0/Extensions/CatExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BYVAM0.Model;
+
+namespace BYVAM0.Extensions
+{
+    internal static class CatExtension
+    {
+        public static string LowerCat(this Cat cat) {
+            return cat.ToString().ToLower();
+        }
+
+        public static string GetCatImgPath(this Cat cat)
+        {
+            return Path.Join(AppContext.BaseDirectory, "Assets", $"{cat.LowerCat()}.png");
+        }
+    }
+}
